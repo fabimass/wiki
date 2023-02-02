7b9f149d6da35ed859d4a0ec962dc2131f8692af
fabimass <massotto.fabian@gmail.com>
2023-02-02 14:18:25 -0300

    implementing search encyclopedia

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 import markdown2
 from . import util
 
 
 def index(request):
+    entriesToShow = util.list_entries()
+    searchFlag = False
+    
+    if request.method == "POST":
+        # Get the user query and check if we have a document that matches
+        userQuery = request.POST["q"]
+        if util.get_entry(userQuery):
+            # Redirect to document page
+            return HttpResponseRedirect(reverse("article", kwargs={'title':userQuery}))
+        else:
+            searchFlag = True
+            for entry in entriesToShow.copy():
+                if userQuery.lower() in entry.lower():
+                    pass
+                else:
+                    entriesToShow.remove(entry)
+        
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+            "entries": entriesToShow,
+            "search": searchFlag
+        })
 
 def article(request, title): 
     entry = util.get_entry(title)
